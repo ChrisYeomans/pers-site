d4022d686436531e0d363bb0a0813c0cccb639ed
ChrisYeomans <chrisj.yeomans@gmail.com>
2020-05-31 20:05:38 +0100

    testing js project load

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-getTxt = function (file_url){
-    $.ajax({
-        url: file_url,
-        success: function (data){
-            return data;
-        }
-    });
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+  }
+
+function printFiles(tag_id) {
+  let files = loadFile('https://raw.githubusercontent.com/ChrisYeomans/pers-site/master/projects/projects_list').split('\n').filter(Boolean);
+  console.log(`[FILES] ${files}`)
+  let projects = files.map(e => loadFile(`https://raw.githubusercontent.com/ChrisYeomans/pers-site/master/projects/${e}.md`));
+  console.log(`[PROJECTS] ${projects}`)
+
+
+  let tag = $(`#${tag_id}`);
+  projects.forEach(e => tag.innerHTML += e); 
+  
 }
-console.log(getTxt('projects/project_list').split('\n'))
