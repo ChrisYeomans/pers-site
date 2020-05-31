8e6496112ef476799ce632ac1a19bed7eb42efaa
ChrisYeomans <chrisj.yeomans@gmail.com>
2020-05-31 20:36:07 +0100

    AJAX project load working

--- js/script.js
+++ js/script.js
@@ -9,14 +9,10 @@ function loadFile(filePath) {
     return result;
   }
 
-function printFiles(tag_id) {
+function getFiles() {
   let files = loadFile('https://raw.githubusercontent.com/ChrisYeomans/pers-site/master/projects/projects_list').split('\n').filter(Boolean);
-  console.log(`[FILES] ${files}`)
+  console.log(`[FILES] ${files}`);
   let projects = files.map(e => loadFile(`https://raw.githubusercontent.com/ChrisYeomans/pers-site/master/projects/${e}.md`));
-  console.log(`[PROJECTS] ${projects}`)
-
-
-  let tag = $(`#${tag_id}`);
-  projects.forEach(e => tag.innerHTML += e); 
-  
+  console.log(`[PROJECTS] ${projects}`);
+  return projects;  
 }
